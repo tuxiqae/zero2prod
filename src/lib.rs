1bbeec65b3ceebde483c1ad538f2fec45b640fd9
Sagi Sarussi <sagis@sagis.dev>
2021-01-25 01:55:09 +0200

    Start integrating Postgres.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,16 @@ use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
@@ -10,9 +20,13 @@ async fn health_check() -> HttpResponse {
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     println!("http://{}", listener.local_addr().unwrap(),);
 
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
